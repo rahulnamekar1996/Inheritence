cc37e4d585e667797dafbdfee92a0d74845694f1
Owner <Owner@DESKTOP-2TPH2RF>
2023-07-12 19:13:57 +0530

    Add project files.

---
+++ Inheritence/Employee.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritence
+{
+    public class Employee
+    {
+        protected int ID;
+        protected string name;
+        protected double bs, hra, ta, da, pf, gross;
+        private static int count;
+
+        public Employee(string name, double bs)
+        {
+            count++;
+            ID = count;
+            this.name = name;
+            this.bs = bs;
+
+        }
+
+        public virtual void CalclateSallary()
+        {
+            hra = bs * 040;
+            ta = bs * 020;
+            da = bs * 0.15;
+            pf = bs * 0.12;
+            gross = (bs + hra + ta + da) - pf;
+        }
+        public override string ToString()
+        {
+            return $"id{ID}, name{name}, Gross{gross}";
+
+
+
+        }
+    }
+    
+    
+    public  class SalesManager : Employee
+    {
+
+        private double comm;
+
+
+        public SalesManager(string name, double bs, double comm):base (name,bs) 
+        {
+            this.comm = comm;
+              
+        }
+        public override void CalclateSallary()
+        {
+            hra = bs * 0.40;
+            ta = bs * 0.20;
+            da = bs * 0.15;
+            pf = bs * 0.12;
+            gross =(bs + hra + ta + da+comm ) - pf;
+        }
+
+        public override string ToString()
+        {
+            return $" id{ID},name{name},Gross{gross}";
+        }
+    }
+        
+
+        
+    
+   
+    
+    
+    
+    
+    
+    
+ }
+
+    
+    
+     
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+      
+        
+
+        
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     
+    
+       
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
---
+++ Inheritence/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SalesManager salesManager = new SalesManager("rahul", 22000, 3500);
+            salesManager.CalclateSallary ();
+            Console.WriteLine(salesManager);
+
+            Employee employee = new Employee("roshan", 20000);
+            employee.CalclateSallary();
+            Console.WriteLine(employee);
+               
+        }
+    }
+}
